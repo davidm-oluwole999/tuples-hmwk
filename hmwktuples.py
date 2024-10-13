3f7d91373946c5101dd512803c7671dcd6ece713
davidm-oluwole999 <davidmoluwole@gmail.com>
2024-10-13 18:04:31 +0100

    Add files via upload

---
+++ hmwktuples.py
@@ -0,0 +1,44 @@
+'''In your class, there are five groups that participated and got prizes in different competitions held in various schools.
+  Your class teacher asks your help to record all the details. Write a program and use a tuple data structure to store each record. '''
+# The program should ask the user to enter details like groupname, Sizeofthegroup, dateofthecompetition, venue, typeofthemedal.
+#Store it as a tuple.
+
+a1=input("Enter the name of your group.")
+a2=input("How many people were in your group?")
+a3=input("What day was the competition? e.g monday  ")
+a4=input("Where was the competition?")
+a5=input("What kind of metal was the metal? e.g silver, none  ")
+group1=(a1, a2, a3, a4, a5)
+print(group1)
+
+b1=input("Enter the name of your group.")
+b2=input("How many people were in your group?")
+b3=input("What day was the competition? e.g monday  ")
+b4=input("Where was the competition?")
+b5=input("What kind of metal was the metal? e.g silver, none  ")
+group2=(b1, b2, b3, b4, b5)
+print(group2)
+
+c1=input("Enter the name of your group.")
+c2=input("How many people were in your group?")
+c3=input("What day was the competition? e.g monday  ")
+c4=input("Where was the competition?")
+c5=input("What kind of metal was the metal? e.g silver, none  ")
+group3=(c1, c2, c3, c4, c5)
+print(group3)
+
+d1=input("Enter the name of your group.")
+d2=input("How many people were in your group?")
+d3=input("What day was the competition? e.g monday  ")
+d4=input("Where was the competition?")
+d5=input("What kind of metal was the metal? e.g silver, none  ")
+group4=(d1, d2, d3, d4, d5)
+print(group4)
+
+e1=input("Enter the name of your group.")
+e2=input("How many people were in your group?")
+e3=input("What day was the competition? e.g monday  ")
+e4=input("Where was the competition?")
+e5=input("What kind of metal was the metal? e.g silver, none  ")
+group5=(e1, e2, e3, e4, e5)
+print(group5)
